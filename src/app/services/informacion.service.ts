dbeb8617d8e31f710c19741d5d3d01c9aba85500
Miguel <fuermiguel@yahoo.es>
2017-08-16 19:08:21 +0100

    servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,30 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any = {};
+  equipo:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosostros:boolean = false;
 
   constructor( public http:Http) { 
-  
+     this.carga_info();
+     this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
-          .subscribe( data =>{
-            console.log(data.json());
-            this.cargada = true ;
-            this.info = data.json();
-          })
+    .subscribe( data =>{
+      //console.log(data.json());
+      this.cargada = true ;
+      this.info = data.json();
+    });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://paginaweb-208e9.firebaseio.com/equipo.json")
+    .subscribe( data =>{
+      console.log(data.json());
+      this.cargada_sobre_nosostros = true ;
+      this.equipo = data.json();
+    });
   }
 }

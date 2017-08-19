22320810fc02d2009eaafafb097a32be91de184b
Miguel <fuermiguel@yahoo.es>
2017-08-19 10:23:01 +0100

    añadimos una animación loading con svg

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -18,8 +18,12 @@ export class ProductosService {
       this.http.get('https://paginaweb-208e9.firebaseio.com/productos_idx.json')
       .subscribe(res => {
         console.log(res.json());
-        this.cargando_productos = false;
-        this.productos = res.json();
-      })
+
+       // setTimeout(()=>{
+          this.cargando_productos = false;
+          this.productos = res.json();
+       // }, 1500)
+       
+      });
   }
 }
